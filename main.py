512e1ace775ad23ee70cc4a90200e9b0ba1cc58c
boypro0409 <68543789+boypro0409@users.noreply.github.com>
2022-02-26 19:55:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -113,8 +113,6 @@ def request_page():
     json_dump = json.dumps(data_set)
     return json_dump
 
-# if __name__ == '__main__':
-#     app.run()
-    
-
-    
+if __name__ == '__main__':
+    app.run()
+    app.debug = True
