8e3603f44eed261ab02f6cdb8bbf4ac07f3f2b36
boypro0409 <boypro0409@gmail.com>
2022-03-02 14:07:43 +0700

    fix deploy heroku

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python
 import os
-import urllib.request
 import cv2
-import argparse
 from flask import *
 from flask_cors import CORS, cross_origin
 from face_detection import select_face
@@ -23,14 +21,14 @@ def index():
 @app.route('/', methods=['POST'])
 def request_page():
     
-    parser = argparse.ArgumentParser(description='FaceSwapApp')
-    # parser.add_argument('--src',  default='imgs/test13.png', help='Path for source image')
-    # parser.add_argument('--dst',  default='imgs/test14.png', help='Path for target image')
-    # parser.add_argument('--out',  default='results/output6_7.jpg', help='Path for storing output images')
-    parser.add_argument('--warp_2d', default=False, action='store_true', help='2d or 3d warp')
-    parser.add_argument('--correct_color', default=False, action='store_true', help='Correct color')
-    parser.add_argument('--no_debug_window', default=False, action='store_true', help='Don\'t show debug window')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='FaceSwapApp')
+    # # parser.add_argument('--src',  default='imgs/test13.png', help='Path for source image')
+    # # parser.add_argument('--dst',  default='imgs/test14.png', help='Path for target image')
+    # # parser.add_argument('--out',  default='results/output6_7.jpg', help='Path for storing output images')
+    # parser.add_argument('--warp_2d', default=False, action='store_true', help='2d or 3d warp')
+    # parser.add_argument('--correct_color', default=False, action='store_true', help='Correct color')
+    # parser.add_argument('--no_debug_window', default=False, action='store_true', help='Don\'t show debug window')
+    # args = parser.parse_args()
     
     user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
     headers={'User-Agent':user_agent} 
@@ -71,7 +69,7 @@ def request_page():
         print('Detect 0 Face !!!')
         exit(-1)
 
-    output = face_swap(src_face, dst_face, src_points, dst_points, dst_shape, dst_img, args)
+    output = face_swap(src_face, dst_face, src_points, dst_points, dst_shape, dst_img)
 
     dir_path = os.path.dirname('results/output6_7.jpg')
     if not os.path.isdir(dir_path):
@@ -114,5 +112,5 @@ def request_page():
     return json_dump
 
 if __name__ == '__main__':
-    app.run()
-    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
